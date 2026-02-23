7f4df4e8efbd8bbf88322ca19e6c1574718fd56e
Kim JinWook <dpscbvls8358@naver.com>
2026-02-23 17:23:12 +0900

    BarrierEffect 수정
    onAction 추가

--- Assets/Script/Card/Effects/BarrierEffect.cs
+++ Assets/Script/Card/Effects/BarrierEffect.cs
@@ -5,21 +5,19 @@ public class BarrierEffect : ICardEffect
     public int amount;
     public string amountFormula;
     public TargetType target;
+    public ICardEffect onAction;
 
     public void Execute(TrainingBattleManager battleManager)
     {
         int finalAmount = amount;
-        
-        if (!string.IsNullOrEmpty(amountFormula))
-        {
-            finalAmount = FormulaEvaluator.Evaluate(amountFormula, battleManager.battleContext, battleManager.playerData);
-        }
 
-        // Barrier targets the player (Self)
-        if (battleManager.playerData != null)
+        // Barrier only applies to player when target is Self.
+        if (target == TargetType.Self && battleManager.playerData != null)
         {
             battleManager.playerData.AddDefense(finalAmount);
             battleManager.UpdateAllUI();
         }
+
+        onAction?.Execute(battleManager);
     }
 }
